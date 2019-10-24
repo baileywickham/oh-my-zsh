48e10c7bc1720dac6278ec908dead83bc320fb0b
baileywickham <baileywickham@gmail.com>
2019-10-24 11:01:01 -0700

    add power

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -44,12 +44,7 @@ for plugin ($plugins); do
 done
 
 # Figure out the SHORT hostname
-if [[ "$OSTYPE" = darwin* ]]; then
-    # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-    SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
-else
-    SHORT_HOST=${HOST/.*/}
-fi
+SHORT_HOST=${HOST/.*/}
 
 # Save the location of the current completion dump file.
 if [ -z "$ZSH_COMPDUMP" ]; then
@@ -92,25 +87,12 @@ done
 unset config_file
 
 # Load the theme
-if [[ "$ZSH_THEME" == "random" ]]; then
-    if [[ "${(t)ZSH_THEME_RANDOM_CANDIDATES}" = "array" ]] && [[ "${#ZSH_THEME_RANDOM_CANDIDATES[@]}" -gt 0 ]]; then
-        themes=($ZSH/themes/${^ZSH_THEME_RANDOM_CANDIDATES}.zsh-theme)
+if [ ! "$ZSH_THEME" = ""  ]; then
+    if [ -f "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme" ]; then
+        source "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme"
+    elif [ -f "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme" ]; then
+        source "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme"
     else
-        themes=($ZSH/themes/*zsh-theme)
-    fi
-    N=${#themes[@]}
-    ((N=(RANDOM%N)+1))
-    RANDOM_THEME=${themes[$N]}
-    source "$RANDOM_THEME"
-    echo "[oh-my-zsh] Random theme '$RANDOM_THEME' loaded..."
-else
-    if [ ! "$ZSH_THEME" = ""  ]; then
-        if [ -f "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme" ]; then
-            source "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme"
-        elif [ -f "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme" ]; then
-            source "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme"
-        else
-            source "$ZSH/themes/$ZSH_THEME.zsh-theme"
-        fi
+        source "$ZSH/themes/$ZSH_THEME.zsh-theme"
     fi
 fi
